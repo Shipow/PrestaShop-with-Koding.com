e6fa2d0ddc1d26d64b6ba135e907323cc3755707
Shipow <shipowlata@gmail.com>
2014-01-14 01:03:18 +0100

    // more stuff

---
+++ javascripts/main.js
@@ -0,0 +1,13 @@
+function TodoCtrl($scope) {
+  $scope.username = "username";
+  $scope.checkUrl = function() {
+    var url = $scope.username+".kd.io/";
+    $.ajax(url, {
+      statusCode: {
+        404: function() {
+          alert('page not found');
+        }
+      }
+    });
+  };
+}
