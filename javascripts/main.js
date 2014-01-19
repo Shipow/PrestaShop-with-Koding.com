b01df1596e3aae6634044d722f747b9ad4f8accf
Kevin Granger <shipowlata@gmail.com>
2014-01-19 16:12:34 +0100

    // update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,37 +1,57 @@
-function Main($scope) {
-  $scope.username = "username";
+var pskoding = angular.module('pskoding', ['ngClipboard']);
+
+pskoding.config(['ngClipProvider', function(ngClipProvider) {
+  ngClipProvider.setPath("javascripts/vendor/zeroclipboard/ZeroClipboard.swf");
+}]);
+
+pskoding.controller('Main', function($scope,$http) {
+  $scope.copyCommand = function(element) {
+    var cmd = $(element).text();
+    $(element).addClass('animated bounce');
+    return cmd;
+  };
   $scope.checkUrl = function() {
-    var url = $scope.username+".kd.io/";
-    $.ajax(url, {
-      statusCode: {
-        404: function() {
-          alert('page not found');
-        }
+    delete $scope.status;
+    var url = "http://"+$scope.username+".kd.io/";
+    $http.jsonp('https://coderbits.com/tools/urlcheck?callback=JSON_CALLBACK&url='+url).
+    success(function(data, status) {
+      $scope.data = data;
+      if (data === 404) {
+        $("#koding-account-error").show();
       }
+    }).
+    error(function(data, status) {
+      $scope.data = data || "Request failed";
     });
   };
-}
+});
 
 $(document).foundation({
   orbit: {
-      animation: 'fade', // Sets the type of animation used for transitioning between slides, can also be 'fade'
-      timer_speed: 1000, // Sets the amount of time in milliseconds before transitioning a slide
-      animation_speed: 500, // Sets the amount of time in milliseconds the transition between slides will last
+      animation: 'fade',
+      timer_speed: 1000,
+      animation_speed: 500,
       navigation_arrows: true,
       slide_number: false,
-      bullets: false, // Does the slider have bullets visible?
-      timer: false, // Does the slider have a timer visible?
-  }
-});
+      bullets: false,
+      timer: false,
+    }
+  });
 
+var progress = 0;
+//var steps = $('#grid > li').length;
 $('#grid > li').waypoint(
-    function() {
+  function() {
     $(this).css('visibility', 'visible').addClass('animated fadeInUp');
-},{offset: '77%'});
+    progress = $(this).data('progress');
+    $('#progress .meter').css('width',progress + '%');
+  },
+  {offset: '77%'}
+);
 
 var disqus_shortname = 'kgranger';
 (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
